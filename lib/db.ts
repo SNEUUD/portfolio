ca246a3291fe16d5a1a80fd4aec96c591ec1c90d
sneuud <87929757+SNEUUD@users.noreply.github.com>
2026-03-31 09:55:32 +0200

    prettier

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
-import Database from 'better-sqlite3';
-import path from 'path';
+import Database from "better-sqlite3";
+import path from "path";
 
-const dbPath = path.join(process.cwd(), 'database.sqlite');
+const dbPath = path.join(process.cwd(), "database.sqlite");
 const db = new Database(dbPath);
 
 export default db;
